28710be3bd727a3abc71ca50611cc67756131442
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-03-02 12:07:32 -0500

    Transfer timer to slot machine

--- app/src/main/java/nl/danielvdspoel/slotmachine/MainActivity.java
+++ app/src/main/java/nl/danielvdspoel/slotmachine/MainActivity.java
@@ -3,118 +3,126 @@ package nl.danielvdspoel.slotmachine;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Gravity;
 import android.widget.Button;
-import android.widget.SeekBar;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView timeView;
-    Button zeroButton;
-    Button upButton;
-    Button downButton;
+    Button runButton;
+    HashMap<Integer, ImageView> slots;
+    HashMap<Integer, Integer> times;
+    HashMap<Integer, CountEvent> events;
 
-    SeekBar speedSeekBar;
-    TextView speedValueText;
+    HashMap<Integer, Drawable> images;
 
-    int time;
-    int speed;
-
-    int changer;
-
-    CountEvent event;
     Handler handler;
 
+    Integer speed = 100;
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timeView = findViewById(R.id.timeView);
+        runButton = findViewById(R.id.runButton);
 
-        upButton = findViewById(R.id.upButon);
-        downButton = findViewById(R.id.downButton);
-        zeroButton = findViewById(R.id.zeroButton);
-        speedSeekBar = findViewById(R.id.speedSeekBar);
-        speedValueText = findViewById(R.id.speedValueText);
-        changer = 1;
+//        time = savedInstanceState != null ? savedInstanceState.getInt("time", 0) : 0;
+//        timeView.setText(String.valueOf(time));
 
-        time = savedInstanceState != null ? savedInstanceState.getInt("time", 0) : 0;
-        speed = savedInstanceState != null ? savedInstanceState.getInt("speed", 1000) : 1000;
+        slots = new HashMap<>();
+        slots.put(0, findViewById(R.id.leftSlotView));
+        slots.put(1, findViewById(R.id.middleSlotView));
+        slots.put(2, findViewById(R.id.rightSlotView));
 
-        timeView.setText(String.valueOf(time));
-        speedValueText.setText(speed + " ms");
+        times = new HashMap<>();
+        times.put(0, 0);
+        times.put(1, 0);
+        times.put(2, 0);
 
-        event = new CountEvent();
+        images = new HashMap<>();
+        images.put(0, getDrawable(R.drawable.cherry));
+        images.put(1, getDrawable(R.drawable.grape));
+        images.put(2, getDrawable(R.drawable.pear));
+        images.put(3, getDrawable(R.drawable.strawberry));
+
+        events = new HashMap<>();
         handler = new Handler();
 
-        speedSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                speed = progress;
-                speedValueText.setText(speed + " ms");
-            }
+        runButton.setOnClickListener(v -> {
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        zeroButton.setOnClickListener(v -> {
-            time = 0;
-            timeView.setText(String.valueOf(time));
-        });
-
-        upButton.setOnClickListener(v -> {
-            if (upButton.getText().equals("Up")) {
-                handler.removeCallbacks(event);
-                downButton.setText("Down");
-
-                upButton.setText("Stop");
-                changer = 1;
-                handler.postDelayed(event, speed);
+            if (runButton.getText().equals("Run!")) {
+                for (Map.Entry<Integer, ImageView> slot : slots.entrySet()) {
+                    CountEvent event = new CountEvent(slot.getKey());
+                    events.put(slot.getKey(), event);
+                    handler.postDelayed(event, speed);
+                }
+                runButton.setText("Stop");
             } else {
-                upButton.setText("Up");
-                handler.removeCallbacks(event);
-            }
-        });
+                runButton.setText("Run!");
 
-        downButton.setOnClickListener(v -> {
-            if (downButton.getText().equals("Down")) {
-                handler.removeCallbacks(event);
-                upButton.setText("Up");
+                if (Objects.equals(times.get(0), times.get(1)) && Objects.equals(times.get(1), times.get(2))) {
+                    Toast t = Toast.makeText(getApplicationContext(), "JACKPOT!!!", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    t.show();
+                } else if (Objects.equals(times.get(0), times.get(1)) || Objects.equals(times.get(0), times.get(2)) || Objects.equals(times.get(1), times.get(2))) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "SEMI-JACKPOT!!!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
 
-                downButton.setText("Stop");
-                changer = -1;
-                handler.postDelayed(event, speed);
-            } else {
-                downButton.setText("Down");
-                handler.removeCallbacks(event);
+                for (Map.Entry<Integer, CountEvent> event : events.entrySet()) {
+                    handler.removeCallbacks(event.getValue());
+                }
             }
         });
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
-        outState.putInt("time", time);
-        outState.putInt("speed", speed);
+        //outState.putInt("time", time);
         super.onSaveInstanceState(outState);
+    }
+
+    public static double getRandom(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("Invalid range [" + min + ", " + max + "]");
+        }
+        return min + Math.random() * (max - min);
     }
     private class CountEvent implements Runnable {
 
+        Integer number;
+        Double speedMultiplier;
+
+        public CountEvent(Integer number) {
+            this.number = number;
+            this.speedMultiplier = getRandom(1, 2);
+        }
+
         @Override
         public void run() {
-            time += changer;
-            timeView.setText(time + "");
-            handler.postDelayed(this, speed);
+            ImageView imageView = slots.get(number);
+            Integer time = times.get(number);
+            if (time < 3) {
+                time += 1;
+            } else {
+                time = 0;
+            }
+            imageView.setImageDrawable(images.get(time));
+            times.put(number, time);
+            handler.postDelayed(this, (long) (speed * speedMultiplier));
         }
     }
 }
